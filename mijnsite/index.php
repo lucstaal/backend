0837111273ba6166f76289fa0ddf86d566e50ab2
lucstaal <jemoeder0722@gmail.com>
2018-02-02 09:57:06 +0100

    Luc

---
+++ mijnsite/index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Luc Staal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/animate.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+          <div class="animated slideInLeft">
+            <a class="navbar-brand" href="index.php"><?php echo "Home"; ?></a>
+          </div>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid bg-1 text-center">
+<h3 class="margin animated slideInLeft"> <?php echo "Wie ben ik?"; ?></h3>
+    <img src="ditbenik.jpg" class="img-responsive img-circle margin animated fadeInUp" style="display:inline" alt="foto" width="350" height="350">
+    <h3 class="animated slideInRight">Luc Staal</h3>
+</div>
+<div class="container-fluid bg-2 text-center">
+    <h3 class="margin1">
+       <?php echo "Wie ben ik?"; ?>
+    </h3>
+    <p>
+        <?php
+            echo "Ik ben Luc Staal en ik doe de opleiding applicatie ontwikkeling op Roc van Twente (Hengelo) .<br>
+        Deze site moest ik maken voor een opdracht met php. We werken nu met Scrum met een groep van vijf mensen.";
+        ?>
+    </p>
+    <img class="onsgroepje" src="onsgroepje.jpg">
+</div>
+<div class="container-fluid bg-3 text-center">
+    <h3 class="margin1">
+        <?php echo "Wat heb ik gemaakt?"; ?>
+    </h3>
+    <p>
+        <?php
+        echo "Hieronder staan de dingen die ik dit semester heb gemaakt.";
+        ?>
+    </p>
+    <a href="https://rocvantwente-my.sharepoint.com/:o:/r/personal/rsk01_rocvantwente_nl/_layouts/15/WopiFrame.aspx?sourcedoc={A19C2C33-D50B-43C7-AFEF-512FEDEB3815}&file=I7AOS2-STD&action=default&RootFolder=%2fpersonal%2frsk01_rocvantwente_nl%2fDocuments%2fKlasnotitieblokken%2fI7AOS2-STD&wdo=6&wdorigin=applauncher,ondcnotebooklist">OneNote Opdrachten</a> <br>
+    <a href="http://jedlcd.000webhostapp.com/">Site van het groepje</a>
+  </div>
+</body>
+</html>
---
+++ weekdagen.php
@@ -0,0 +1,3 @@
+<?php
+echo "Maandag <br> Dinsdag <br> Woensdag <br> Donderdag <br> Vrijdag <br>";
+ ?>
